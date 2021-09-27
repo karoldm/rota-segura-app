51e37dcbc774ce58cf9ae63afc05034679d78532
karol marques <karoldm1579@gmail.com>
2021-09-26 22:16:54 -0300

    clear map to update route

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -38,126 +38,111 @@ class HomePageState extends State<HomePage> {
               if (snapshot.hasData) {
                 dataUser = snapshot.data!;
                 return Scaffold(
-                  backgroundColor: Colors.white,
-                  drawer: Drawer(
-                      child: ListView(children: <Widget>[
-                    DrawerHeader(
-                      child: Text(
-                        'Bem Vindo(a) ' + dataUser['name'],
-                        textAlign: TextAlign.center,
-                        style: TextStyle(color: Colors.white, fontSize: 20.0),
+                    backgroundColor: Colors.white,
+                    drawer: Drawer(
+                        child: ListView(children: <Widget>[
+                      DrawerHeader(
+                        child: Text(
+                          'Bem Vindo(a) ' + dataUser['name'],
+                          textAlign: TextAlign.center,
+                          style: TextStyle(color: Colors.white, fontSize: 20.0),
+                        ),
+                        decoration: BoxDecoration(
+                            color: Color(0xff00AAFF),
+                            image: DecorationImage(
+                                image: AssetImage("images/mapMarker.png"),
+                                fit: BoxFit.cover,
+                                alignment: Alignment.topCenter)),
                       ),
-                      decoration: BoxDecoration(
-                          color: Color(0xff00AAFF),
-                          image: DecorationImage(
-                              image: AssetImage("images/mapMarker.png"),
-                              fit: BoxFit.cover,
-                              alignment: Alignment.topCenter)),
-                    ),
-                    ListTile(
-                      leading: Icon(Icons.settings, color: Colors.black),
-                      title: Text('configurações'),
-                      onTap: () => Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              //mandando usuário para a página de perfil
-                              builder: (context) => EditPage())),
-                    ),
-                    ListTile(
-                      leading: Icon(Icons.logout, color: Colors.black),
-                      title: Text('sair'),
-                      onTap: () => userModel.logout(() {
-                        Navigator.pushReplacement(
+                      ListTile(
+                        leading: Icon(Icons.settings, color: Colors.black),
+                        title: Text('configurações'),
+                        onTap: () => Navigator.push(
                             context,
                             MaterialPageRoute(
                                 //mandando usuário para a página de perfil
-                                builder: (context) => LoginPage()));
-                      }, () {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                            const SnackBar(
-                                backgroundColor: Colors.red,
-                                content: Text('Erro ao sair!')));
-                      }),
-                    )
-                  ])),
-                  appBar: AppBar(
-                    centerTitle: true,
-                    leading: Builder(
-                        builder: (conext) => IconButton(
-                            onPressed: () => Scaffold.of(conext).openDrawer(),
-                            icon: Icon(
-                              Icons.menu,
-                              size: 30,
-                            ))),
-                    actions: <Widget>[
-                      Container(
-                        padding: const EdgeInsets.all(5),
-                        child: Icon(
-                          Icons.help,
-                          color: Color(0xff005783),
-                          size: 40,
-                        ),
+                                builder: (context) => EditPage())),
                       ),
-                    ],
-                    backgroundColor: Color(0xff3dbeff),
-                    title: Text(
-                      "Rota Segura",
-                      style: TextStyle(fontSize: 25),
+                      ListTile(
+                          leading: Icon(
+                            Icons.map,
+                            color: Colors.black,
+                          ),
+                          title: Text('atualizar rota'),
+                          onTap: () {
+                            mapModel.clearMap();
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    //mandando usuário para a página de cadastro de rota
+                                    builder: (context) => RegisterRoutePage()));
+                          }),
+                      ListTile(
+                        leading: Icon(Icons.logout, color: Colors.black),
+                        title: Text('sair'),
+                        onTap: () => userModel.logout(() {
+                          Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                  //mandando usuário para a página de perfil
+                                  builder: (context) => LoginPage()));
+                        }, () {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                  backgroundColor: Colors.red,
+                                  content: Text('Erro ao sair!')));
+                        }),
+                      )
+                    ])),
+                    appBar: AppBar(
+                      centerTitle: true,
+                      leading: Builder(
+                          builder: (conext) => IconButton(
+                              onPressed: () => Scaffold.of(conext).openDrawer(),
+                              icon: Icon(
+                                Icons.menu,
+                                size: 30,
+                              ))),
+                      actions: <Widget>[
+                        Container(
+                          padding: const EdgeInsets.all(5),
+                          child: Icon(
+                            Icons.help,
+                            color: Color(0xff005783),
+                            size: 40,
+                          ),
+                        ),
+                      ],
+                      backgroundColor: Color(0xff3dbeff),
+                      title: Text(
+                        "Rota Segura",
+                        style: TextStyle(fontSize: 25),
+                      ),
                     ),
-                  ),
-                  body: Stack(
-                    children: <Widget>[
-                      Container(
-                          child: FutureBuilder(
-                              future: mapModel.getPolyline(),
-                              builder: (context,
-                                  AsyncSnapshot<Set<Polyline>> snapshot) {
-                                if (snapshot.hasData) {
-                                  return (GoogleMap(
-                                    onMapCreated: mapModel.onMapCreated,
-                                    polylines: mapModel.polyline,
-                                    initialCameraPosition: CameraPosition(
-                                      target: _initialCamera,
-                                    ),
-                                    zoomControlsEnabled: false,
-                                    mapToolbarEnabled: false,
-                                    myLocationButtonEnabled: true,
-                                  ));
-                                } else {
-                                  return (Center(
-                                      child: CircularProgressIndicator()));
-                                }
-                              })),
-                      Align(
-                          alignment: Alignment.bottomCenter,
-                          child: Padding(
-                            padding: const EdgeInsets.all(5.0),
-                            child: Row(
-                              children: <Widget>[
-                                Button(
-                                  title: "cadastrar rota",
-                                  colors: [0xff004F77, 0xff3DBEFF],
-                                  function: () {
-                                    Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                            //mandando usuário para a página de cadastro de rota
-                                            builder: (context) =>
-                                                RegisterRoutePage()));
-                                  },
-                                ),
-                                SizedBox(width: 10),
-                                Button(
-                                  title: "Compartilhar rota",
-                                  colors: [0xffDB0000, 0xffFF7272],
-                                  function: () {},
-                                )
-                              ],
-                            ),
-                          ))
-                    ],
-                  ),
-                );
+                    body: Container(
+                      child: Stack(children: <Widget>[
+                        FutureBuilder(
+                            future: mapModel.getPolyline(),
+                            builder: (context,
+                                AsyncSnapshot<Set<Polyline>> snapshot) {
+                              if (snapshot.hasData) {
+                                return (GoogleMap(
+                                  onMapCreated: mapModel.onMapCreated,
+                                  polylines: mapModel.polyline,
+                                  initialCameraPosition: CameraPosition(
+                                    target: _initialCamera,
+                                  ),
+                                  zoomControlsEnabled: false,
+                                  mapToolbarEnabled: false,
+                                  myLocationButtonEnabled: true,
+                                ));
+                              } else {
+                                return (Center(
+                                    child: CircularProgressIndicator()));
+                              }
+                            }),
+                      ]),
+                    ));
               } else {
                 return Center(child: CircularProgressIndicator());
               }
