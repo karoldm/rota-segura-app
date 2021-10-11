9eb3c15e131ee1f365719accc404a6284205a172
karolyne <karoldm1579@gmail.com>
2021-10-11 00:56:38 -0300

    add app bar to user route page

--- lib/screens/admin/userRoute.dart
+++ lib/screens/admin/userRoute.dart
@@ -1,9 +1,13 @@
 // ignore_for_file: import_of_legacy_library_into_null_safe
 
 import 'package:flutter/material.dart';
+
+//models
 import 'package:rota_segura_app/models/admin.dart';
 
 //screens
+import 'package:rota_segura_app/screens/admin/help.dart';
+import 'package:rota_segura_app/screens/admin/home.dart';
 import 'package:rota_segura_app/screens/admin/userInfoMarker.dart';
 
 //libraries
@@ -26,6 +30,38 @@ class _UserRoutePageState extends State<UserRoutePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          color: Colors.white,
+          onPressed: () => Navigator.pushReplacement(
+              context, MaterialPageRoute(builder: (context) => AdminHome())),
+        ),
+        elevation: 0.0,
+        actions: <Widget>[
+          Container(
+            padding: const EdgeInsets.all(5),
+            child: IconButton(
+              icon: Icon(
+                Icons.help,
+                size: 40.0,
+              ),
+              color: Color(0xff005783),
+              onPressed: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      //mandando usuário para a página de perfil
+                      builder: (context) => AdminHelpPage())),
+            ),
+          ),
+        ],
+        backgroundColor: Color(0xff3dbeff),
+        title: Text(
+          "Rota Segura",
+          style: TextStyle(fontSize: 25),
+        ),
+        centerTitle: true,
+      ),
       body: ScopedModelDescendant<AdminModel>(builder: (context, child, model) {
         return Container(
             child: GoogleMap(
